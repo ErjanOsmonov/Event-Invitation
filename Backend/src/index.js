0a7c1387e4dd2a887b786bbfa2f9568f5a24c76f
Arslan <arslansopakunov@gmail.com>
2025-03-23 03:48:12 +0600

    feat: add CORS support for frontend integration

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const controllers = require('./controllers/index.js');
+const cors = require('cors'); // Добавляем CORS
+const guestRoutes = require('./routes/guestRoutes');
+const errorHandler = require('./middlewares/errorHandler');
+const db = require('./utils/config'); // eslint-disable-line no-unused-vars
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.post('/register', controllers.registerGuest);
-app.get('/guests', controllers.getGuests);
-app.post('/wishes', controllers.addWish);
-app.get('/wishes', controllers.getWishes);
+app.use(cors({ origin: 'http://localhost:8080', credentials: true })); // Разрешаем запросы с фронтенда
+app.use('/api', guestRoutes);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
